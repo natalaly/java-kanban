018498711bcd0b932cf3c5bc3418d96cba528191
nataliiaiallina <natalia.iallina@gmail.com>
2024-04-03 20:15:21 -0400

    Add JUnit library

--- src/Main.java
+++ src/Main.java
@@ -23,55 +23,7 @@ public class Main {
   public static void main(String[] args) {
     t = Managers.getDefault(); //start task tracker
 
-    getReadyToAddAll(); //create all tasks, epics and subtasks with titles, descriptions, and statuses NEW
-    System.out.println("Task View History:(Should be blank) " + t.getHistory());
-    addAll();
-    System.out.println("Apply addAll();....");
-    System.out.println("Task View History:(Should be blank) " + t.getHistory());
-
-    System.out.println("All Tasks(should be 2): " + t.getAllTasks().size());
-    System.out.println("All Epics(should be 2): " + t.getAllEpics().size());
-    System.out.println("All SubTasks(should be 3): " + t.getAllSubtasks().size());
-    System.out.println("Subtasks in Epic 1 (id = 3): " + t.getSubtasksByEpicId(3));
-    System.out.println("Task View History:(Should be blank) " + t.getHistory());
-    System.out.println();
-
-//    t.getTaskById(1);
-    for (int i = 0; i < 10; i++) {
-      t.getTaskById(1);
-    }
-    System.out.println("t.getTaskById(1); X 10...");
-    System.out.println("Task View History: " + t.getHistory());
-    System.out.println("Task View History size(should be 10): " + t.getHistory().size());
-    System.out.println();
-
-//    //delete task that had been viewed before and check history
-//    t.deleteTask(1);
-//    System.out.println("Task View History: " + t.getHistory());
-    System.out.println();
-    //
-    System.out.println("t.getEpicById(3)....");
-    System.out.println(t.getEpicById(3));
-    System.out.println("Task View History: " + t.getHistory());
-    System.out.println("Task View History size(should be 10): " + t.getHistory().size());
-    System.out.println();
-    //create new subtask for Epic1 (id=3)
-    System.out.println("Add new subtask to the epic 1 (id = 3), update it with status IN_PROGRESS");
-    Subtask subtask = new Subtask();
-    subtask.setTitle("qwqwe");
-    subtask.setDescription("rer");
-    subtask.setStatus(TaskStatus.NEW);
-    subtask.setEpicId(3);
-    t.addSubtask(subtask);
-    subtask.setStatus(TaskStatus.IN_PROGRESS);
-    t.updateSubtask(subtask);
-    System.out.println("Task View History: " + t.getHistory());
-    System.out.println("Subtasks in Epic 1 (id = 3): " + t.getSubtasksByEpicId(3));
-    System.out.println("Task View History: " + t.getHistory());
-
-
-
-
+    checkHowHistoryWorks();
 
 //    test1AddTask();
 //    test2AddEpic();
@@ -372,6 +324,54 @@ public class Main {
       result = result + task + "\n";
     }
     return result;
+  }
+
+  static void checkHowHistoryWorks() {
+    getReadyToAddAll(); //create all tasks, epics and subtasks with titles, descriptions, and statuses NEW
+    System.out.println("Task View History:(Should be blank) " + t.getHistory());
+    addAll();
+    System.out.println("Apply addAll();....");
+    System.out.println("Task View History:(Should be blank) " + t.getHistory());
+
+    System.out.println("All Tasks(should be 2): " + t.getAllTasks().size());
+    System.out.println("All Epics(should be 2): " + t.getAllEpics().size());
+    System.out.println("All SubTasks(should be 3): " + t.getAllSubtasks().size());
+    System.out.println("Subtasks in Epic 1 (id = 3): " + t.getSubtasksByEpicId(3));
+    System.out.println("Task View History:(Should be blank) " + t.getHistory());
+    System.out.println();
+
+//    t.getTaskById(1);
+    for (int i = 0; i < 10; i++) {
+      t.getTaskById(1);
+    }
+    System.out.println("t.getTaskById(1); X 10...");
+    System.out.println("Task View History: " + t.getHistory());
+    System.out.println("Task View History size(should be 10): " + t.getHistory().size());
+    System.out.println();
+
+//    //delete task that had been viewed before and check history
+//    t.deleteTask(1);
+//    System.out.println("Task View History: " + t.getHistory());
+    System.out.println();
+    //
+    System.out.println("t.getEpicById(3)....");
+    System.out.println(t.getEpicById(3));
+    System.out.println("Task View History: " + t.getHistory());
+    System.out.println("Task View History size(should be 10): " + t.getHistory().size());
+    System.out.println();
+    //create new subtask for Epic1 (id=3)
+    System.out.println("Add new subtask to the epic 1 (id = 3), update it with status IN_PROGRESS");
+    Subtask subtask = new Subtask();
+    subtask.setTitle("qwqwe");
+    subtask.setDescription("rer");
+    subtask.setStatus(TaskStatus.NEW);
+    subtask.setEpicId(3);
+    t.addSubtask(subtask);
+    subtask.setStatus(TaskStatus.IN_PROGRESS);
+    t.updateSubtask(subtask);
+    System.out.println("Task View History: " + t.getHistory());
+    System.out.println("Subtasks in Epic 1 (id = 3): " + t.getSubtasksByEpicId(3));
+    System.out.println("Task View History: " + t.getHistory());
   }
 
 }
---
+++ test/main/java/ru/yandex/practicum/tasktracker/model/TaskTest.java
@@ -0,0 +1,50 @@
+package main.java.ru.yandex.practicum.tasktracker.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+
+  @Test
+  void getTitle() {
+  }
+
+  @Test
+  void setTitle() {
+  }
+
+  @Test
+  void getDescription() {
+  }
+
+  @Test
+  void setDescription() {
+  }
+
+  @Test
+  void getStatus() {
+  }
+
+  @Test
+  void setStatus() {
+  }
+
+  @Test
+  void getId() {
+  }
+
+  @Test
+  void setId() {
+  }
+}
